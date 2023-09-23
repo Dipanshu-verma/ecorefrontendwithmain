fef20968093738ccd05c1c404632c39617c3b81d
Dipanshu-verma <vermadipanshu444@gmail.com>
2023-09-23 19:27:52 +0530

    ecorefront

--- src/firebaseauth.js
+++ src/firebaseauth.js
@@ -4,13 +4,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBq8Cea4Po5LHKoc0B3pOf3n2D_Nm-huMU",
-    authDomain: "ecommerc-c7729.firebaseapp.com",
-    projectId: "ecommerc-c7729",
-    storageBucket: "ecommerc-c7729.appspot.com",
-    messagingSenderId: "273676323472",
-    appId: "1:273676323472:web:cbcd2982364c395d4a2615"
-  };
+  apiKey: "AIzaSyAcSHMpkPYazrAZvjcLXfvrja08a-7VroU",
+  authDomain: "my-tube-clone-7eda2.firebaseapp.com",
+  projectId: "my-tube-clone-7eda2",
+  storageBucket: "my-tube-clone-7eda2.appspot.com",
+  messagingSenderId: "283814602509",
+  appId: "1:283814602509:web:82064cde7a9b50d755f244"
+};
 
   
   firebase.initializeApp(firebaseConfig)
